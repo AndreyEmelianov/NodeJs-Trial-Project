f5d37a45b02bbd49ae0c970982bb52b87111c4e1
Andrey Emelianov <621077@mail.ru>
2023-06-21 21:16:23 +0300

    реализовал класс обработки ошибок и описал его интрефейс

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { Server } from 'http';
 
 import { LoggerService } from './logger/logger.service';
 import { UsersController } from './users/users.controller';
+import { ExeptionFilter } from './errors/exeption.filter';
 
 export class App {
 	app: Express;
@@ -10,20 +11,31 @@ export class App {
 	port: number;
 	logger: LoggerService;
 	userController: UsersController;
+	exeptionFilter: ExeptionFilter;
 
-	constructor(logger: LoggerService, userController: UsersController) {
+	constructor(
+		logger: LoggerService,
+		userController: UsersController,
+		exeptionFilter: ExeptionFilter
+	) {
 		this.app = express();
 		this.port = 8000;
 		this.logger = logger;
 		this.userController = userController;
+		this.exeptionFilter = exeptionFilter;
 	}
 
 	useRoutes() {
 		this.app.use('/users', this.userController.router);
 	}
 
+	useExeptionsFilters() {
+		this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
+	}
+
 	public async init() {
 		this.useRoutes();
+		this.useExeptionsFilters();
 		this.server = this.app.listen(this.port);
 		this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
 	}
