73b0b10c5fcb7ba7c766f0fcdcb53c67a5d59f1f
Andrey Emelianov <621077@mail.ru>
2023-06-28 18:18:56 +0300

    создал entity класс User подключил bcryptjs для кеширования пароля, отправил тестовый запрос, всё отрабатывает корректно

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import { TYPES } from '../types';
 import { IUsersController } from './users.controller.interface';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
+import { User } from './user.entity';
 @injectable()
 export class UsersController extends BaseController implements IUsersController {
 	constructor(@inject(TYPES.ILogger) private LoggerService: ILogger) {
@@ -20,12 +21,16 @@ export class UsersController extends BaseController implements IUsersController 
 	}
 
 	login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
-		console.log(req.body);
 		this.ok(res, 'login');
 	}
 
-	register(req: Request<{}, {}, UserRegisterDto>, res: Response, next: NextFunction): void {
-		console.log(req.body);
-		this.ok(res, 'register');
+	async register(
+		{ body }: Request<{}, {}, UserRegisterDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const newUser = new User(body.email, body.name);
+		await newUser.setPassword(body.password);
+		this.ok(res, newUser);
 	}
 }
