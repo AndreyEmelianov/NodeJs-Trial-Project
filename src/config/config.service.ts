790a861e1a6e711326dd451c6b6b0993c1584ce0
Andrey Emelianov <621077@mail.ru>
2023-06-28 19:18:50 +0300

    подключил dotenv и реализовал конфиг сервис

---
+++ src/config/config.service.ts
@@ -0,0 +1,23 @@
+import { DotenvConfigOutput, DotenvParseOutput, config } from 'dotenv';
+import { inject, injectable } from 'inversify';
+
+import { IConfigService } from './config.service.interface';
+import { ILogger } from '../logger/logger.interface';
+import { TYPES } from '../types';
+
+@injectable()
+export class ConfigService implements IConfigService {
+	private config: DotenvParseOutput;
+
+	constructor(@inject(TYPES.ILogger) private logger: ILogger) {
+		const result: DotenvConfigOutput = config();
+		if (result.error) {
+			this.logger.error('Не удалось прочитать файл .env или он отсутствует');
+		} else {
+			this.config = result.parsed as DotenvParseOutput;
+		}
+	}
+	get<T extends number | string>(key: string): T {
+		return this.config[key] as T;
+	}
+}
