7d248a1474e3f968b6aa724c0a7f8cd19d84010f
Andrey Emelianov <621077@mail.ru>
2023-06-29 12:33:54 +0300

    добавил методы userRepository в userService, проверил как отрабатывает взаимодействие с базой данных, всё работает корректно

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,20 +6,29 @@ import { User } from './user.entity';
 import { IUsersService } from './users.service.interface';
 import { IConfigService } from '../config/config.service.interface';
 import { TYPES } from '../types';
+import { IUsersRepository } from './users.repository.interface';
+import { UserModel } from '@prisma/client';
 
 @injectable()
 export class UserService implements IUsersService {
-	constructor(@inject(TYPES.ConfigService) private configService: IConfigService) {}
-	async createUser({ email, name, password }: UserRegisterDto): Promise<User | null> {
+	constructor(
+		@inject(TYPES.ConfigService) private configService: IConfigService,
+		@inject(TYPES.UsersRepository) private usersRepository: IUsersRepository,
+	) {}
+	async createUser({ email, name, password }: UserRegisterDto): Promise<UserModel | null> {
 		// создали нового пользователя
 		const newUser = new User(email, name);
 		const salt = this.configService.get('SALT');
 		await newUser.setPassword(password, Number(salt));
 
 		// проверка что он есть?
+		const existedUser = await this.usersRepository.find(email);
 		// если есть возвращаем null
+		if (existedUser) {
+			return null;
+		}
 		// если нет - создаём
-		return null;
+		return this.usersRepository.create(newUser);
 	}
 
 	async validateUser(dto: UserLoginDto): Promise<boolean> {
