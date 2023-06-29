4ca121d840685a48623a029350471a15dfcbb823
Andrey Emelianov <621077@mail.ru>
2023-06-29 15:46:55 +0300

    реализовал e2e тест на то что пользователь уже есть в базе и не может быть зарегистрирован

--- src/main.ts
+++ src/main.ts
@@ -32,14 +32,14 @@ export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
 	bind<App>(TYPES.Application).to(App);
 });
 
-function bootstrap(): IBootstrapReturt {
+async function bootstrap(): Promise<IBootstrapReturt> {
 	const appContainer = new Container();
 	appContainer.load(appBindings);
 
 	const app = appContainer.get<App>(TYPES.Application);
-	app.init();
+	await app.init();
 
 	return { app, appContainer };
 }
 
-export const { app, appContainer } = bootstrap();
+export const boot = bootstrap();
